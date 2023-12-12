f0cdbb39d0f6f817d624312b383c8cc4d7421732
Arman-Kabir <armankabir8@gmail.com>
2023-12-12 16:53:24 +0600

    google strategy

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20');
 passport.use(
     new GoogleStrategy({
         // options for google strategy
+        clientID:process.env.CLIENT_ID,
+        clientSECRET:process.env.CLIENT_SECRET
     }),()=>{
         // passport callback function
     }
