4eb28168934aa6b9018cc4dc2bc19d22bd6fa809
Arman-Kabir <armankabir8@gmail.com>
2023-12-13 17:13:11 +0600

    mongoose saved user on db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,16 +1,24 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/User-model');
 
 passport.use(
     new GoogleStrategy({
         // options for google strategy
-        callbackURL:'/auth/google/redirect',
-        clientID:keys.google.clientID,
-        clientSecret:keys.google.clientSecret
-    },(accessToken,refreshToken,profile,done)=>{
+        callbackURL: '/auth/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
         console.log('passport callback func fire');
         console.log(profile);
+
+        new User({
+            username: profile.displayName,
+            googleId: profile.id
+        }).save().then((newUser) => {
+            console.log('new user created' + newUser);
+        })
     })
 )
